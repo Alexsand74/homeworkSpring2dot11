9310156ce276398d4bfc5ea78c78b31f9f1b056a
Alexsand74 <1974alexsand@gmail.com>
2022-09-16 20:55:12 +0300

    Домашняя работа по уроку 2.11. Spring

---
+++ src/main/java/home/work/homeworkSpring2dot11/HomeworkSpring2dot11Application.java
@@ -0,0 +1,13 @@
+package home.work.homeworkSpring2dot11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpring2dot11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpring2dot11Application.class, args);
+	}
+
+}
---
+++ src/test/java/home/work/homeworkSpring2dot11/HomeworkSpring2dot11ApplicationTests.java
@@ -0,0 +1,13 @@
+package home.work.homeworkSpring2dot11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpring2dot11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
